f29c4467e73956a78c833a0f13bf53e67c8e1de4
Mark Halstead <mark.d.halstead@gmail.com>
2021-05-20 17:14:34 -0600

    Column Full Fix

--- first.py
+++ first.py
@@ -15,7 +15,7 @@ def print_board(board):
 
 GAME_OVER = False
 player_turn = 1
-plays = {"Player 1":[],"Player 2":[]}
+# plays = {"Player 1":[],"Player 2":[]}
 
 # def verify_input(input):
 
@@ -31,41 +31,87 @@ plays = {"Player 1":[],"Player 2":[]}
 #     # return print("Error")
     #  
 
+
+# 
+# WORK ON NEXT
+#when column full keep player turn. 
+# 
+
+def check_win(board):
+#     # check horizontal for 4 across
+    for r in board:
+        for c in range(len(r)-3):
+            if r[c] == 1 or r[c] == 2:
+                if r[c] == r[c+1] == r[c+2] == r[c+3] :
+                    print("Player ", r[c]," wins!")
+                    return True
+    # vertical check
+    for r in range(len(board) - 3):
+        for c in range(len(board[r])):
+            if board[r][c] == 1 or board[r][c] == 2:
+                if board[r][c] == board[r+1][c] == board[r+2][c] == board[r+3][c]:
+                    print(print("Player ", board[r][c]," wins!"))
+                    return True
+    # positive diagonal
+    for r in range(len(board) - 3):
+        for c in range(len(board[r] - 3)):
+            if board[r][c] == 1 or board[r][c] == 2:
+                if board[r][c] == board[r+1][c+1] == board[r+2][c+2] == board[r+3][c+3]:
+                    print(print("Player ", board[r][c]," wins!"))
+                    return True
+    # negative diagonal 
+    for r in range(len(board)-3):
+        for c in range(len(board[r])-3):
+            if board[r][c] == 1 or board[r][c] == 2:
+                if board[r][c] == board[r-1][c-1] == board[r-2][c-2] == board[r-3][c-3]:
+                    print(print("Player ", board[r][c]," wins!"))
+                    return True
+                    
+def is_valid_column(board,col):
+    if int(board[0][col]) == 0:
+        return True
+    else:
+        return False
+
+print(is_valid_column(board,2))
 def drop_piece(board,col,player):
-    if is_space_available(board,col):
+    if is_valid_column(board,col):
         for r in range(len(board)-1,-1,-1):
             if board[r][col] == 0:
                 board[r][col] = player
-                break
+                return True
     else:
         print("Column full. Please try another column.")
-
+        return False
+        
 def input_column(board, player):
     player_input = 0
     while player_input >= 8 or player_input <= 0:
-        try:    
-            player_input = int(input("Player "+str(player)+" Choose a column (1-7):"))
-            # print(player_input)
-            if player_input > 0 and player_input < 8:
-                # print(player)
-                player_input -= 1
-                drop_piece(board,player_input,player)
-                print(board)
+    
+            try:    
+                player_input = int(input("Player "+str(player)+" Choose a column (1-7):"))
+                # print(player_input)
+                if player_input > 0 and player_input < 8:
+                    # print(player)
+                    player_input -= 1
+                    if(drop_piece(board,player_input,player)):
+                        print(board)
+                        return True
+                        # break
+                    else:
+                        return False
+                else:
+                    print("Please input integer between 1-7")
                 
-            else:
-              print("Please input integer between 1-7")
-            
-
-        
-        except:
-            print("Please input integer between 1-7, inclusive")
+            except:
+                print("Please input integer between 1-7, inclusive")
 
             
-def is_space_available(board,col):
-    for r in board:
-            if r[col] == 0:
-                return True
-    return False
+# def is_space_available(board,col):
+#     for r in board:
+#             if r[col] == 0:
+#                 return True
+#     return False
 
 
 
@@ -91,18 +137,25 @@ def is_space_available(board,col):
 
 
 def play(GAME_OVER,player_turn,board):
-    # print(create_board())
+    print(board)
     while not GAME_OVER:
         # Get input for player 1
        
         if player_turn == 1:
-            input_column(board,1)
-            player_turn = 2
+            if input_column(board,1) == True:
+                player_turn = 2
+                check_win(board)
+
+            else:
+                # input_column(board,1)
+                player_turn = 1
 
         if player_turn == 2:
-            input_column(board,2)
-            player_turn = 1
-
+            if input_column(board,2) == True:
+                player_turn = 1
+                check_win(board)
+            else:
+                player_turn = 2
         
 
 # print(print_board(board))
