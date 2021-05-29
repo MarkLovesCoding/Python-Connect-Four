20ee0ef0e5b8bcc2220963377dcc8e8d31408e2c
Mark Halstead <mark.d.halstead@gmail.com>
2021-05-29 14:33:33 -0600

    Add comments

--- connectfour.py
+++ connectfour.py
@@ -23,38 +23,46 @@ intro_banner="""
   #####  ####### #     # #     # #######  #####     #       #       #######  #####  #     # 
 \n\nHow Many Players (1 or 2)?"""
 
+# print board with columns
 def print_board(board):
     print(board)
     print("  1  2  3  4  5  6  7  ")
 
+# random column for computer move (basic easy 1 player game)
 def computer_player_move():
     column = random.randint(0,6)
     return column
-    
+
+# standard starting
 num_players = 2
 
+# play game
 def intro():
     board = create_board()
     how_many = input(intro_banner)
+    # 2 player game
     if how_many == "2":
         play_two_player(game_over,player_turn,board)
+    # 1 player game
     if how_many == "1":
         num_players = 1
-        # print("Too bad, there is only a 2 player version.")
         play_one_player(game_over,player_turn,board)
+    # default to 2 player
     else:
         print("You typed:",how_many, ". \nThat's not a 1 or a 2, silly.\nLet's play a two player game.")
         play_two_player(game_over,player_turn,board)
 
+# ask to play again, then run intro()
 def play_again():
     play_again_input = input("Play again? (Y or N)")
+    board = create_board()
     if play_again_input == "Y" or play_again_input == "y":
-        board = create_board()
         return intro()
     else:
         print("Thanks for the game.")
         return False
 
+# check winning orientations
 def check_win(board):
 #     # check horizontal for 4 across
     for r in board:
@@ -85,13 +93,15 @@ def check_win(board):
                 if board[r][c] == board[r+1][c-1] == board[r+2][c-2] == board[r+3][c-3]:
                     print(print("Player ", str(board[r][c])[0]," wins!"))
                     return True
-                    
+                  
+# Check if top row is empty or full  
 def is_valid_column(board,col):
     if int(board[0][col]) == 0:
         return True
     else:
         return False
 
+# drop piece to bottom most available row in column
 def drop_piece(board,col,player):
     if is_valid_column(board,col):
         for r in range(len(board)-1,-1,-1):
@@ -102,6 +112,7 @@ def drop_piece(board,col,player):
         print("Column full. Please try another column.")
         return False
 
+# drop computer piece until in valid column
 def drop_piece_comp(board,col,player):
     if is_valid_column(board,col):
             for r in range(len(board)-1,-1,-1):
@@ -112,12 +123,14 @@ def drop_piece_comp(board,col,player):
         col = computer_player_move()
         drop_piece_comp(board,col,player)
 
-
+# player inputs column
 def input_column(board, player):
     player_input = 0
     while player_input >= 8 or player_input <= 0:
+            # Handle input error if not an integer
             try:    
                 player_input = int(input("Player "+str(player)+" Choose a column (1-7):"))
+                # Check input between/including 1-7
                 if player_input > 0 and player_input < 8:
                     player_input -= 1
                     if(drop_piece(board,player_input,player)):
@@ -127,16 +140,15 @@ def input_column(board, player):
                         return False
                 else:
                     print("Please input integer between 1-7")
-                
+            # on integer input error
             except:
                 print("Please input integer between 1-7, inclusive")
 
   
-
+# run two player game logic
 def play_two_player(game_over,player_turn,board):
     print_board(board)
     while not game_over:
-       
         if player_turn == 1:
             if input_column(board,1) == True:
                 player_turn = 2
@@ -156,6 +168,7 @@ def play_two_player(game_over,player_turn,board):
             else:
                 player_turn = 2
 
+# run single player game logic
 def play_one_player(game_over,player_turn,board):
     print_board(board)
     while not game_over:
@@ -182,7 +195,11 @@ def play_one_player(game_over,player_turn,board):
                 player_turn = 2
 
 # Play game
-# computer_player_move()
 intro()
 
 
+# IMPROVEMENTS TO BE MADE
+# 1. re-factor game logic based on number of players so it is DRY
+# 2. create option to start as player 1 or 2 in single player
+# 3. make AI at least competitive
+# 4. practice with classes. Save player profile to local memory as class object?
