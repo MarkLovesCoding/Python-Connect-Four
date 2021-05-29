4b345256b77f1c475cfa58c2120a8a4616867a61
Mark Halstead <mark.d.halstead@gmail.com>
2021-05-29 12:50:39 -0600

    Add single player functionality

--- connectfour.py
+++ connectfour.py
@@ -1,22 +1,18 @@
 import numpy
+import random
+
+# INITIALIZE ROWS/COLUMNS VARS
 ROWS = 6
 COLUMNS = 7
-# Columns,  6 x 7
+
 def create_board():
     board = numpy.zeros((ROWS,COLUMNS))
     return board
 
-
-board = create_board()
-def print_board(board):
-    print(board)
-    print("  1  2  3  4  5  6  7  ")
-
-
-
+# INITIALIZE OTHER VARS
 game_over = False
 player_turn = 1
-
+# board = create_board()
 intro_banner="""
   #####  ####### #     # #     # #######  #####  #######    ####### ####### #     # ######  
  #     # #     # ##    # ##    # #       #     #    #       #       #     # #     # #     # 
@@ -25,24 +21,36 @@ intro_banner="""
  #       #     # #   # # #   # # #       #          #       #       #     # #     # #   #   
  #     # #     # #    ## #    ## #       #     #    #       #       #     # #     # #    #  
   #####  ####### #     # #     # #######  #####     #       #       #######  #####  #     # 
-                                                                                            
-                                                                                            \n\nHow Many Players (1 or 2)?"""
+\n\nHow Many Players (1 or 2)?"""
+
+def print_board(board):
+    print(board)
+    print("  1  2  3  4  5  6  7  ")
+
+def computer_player_move():
+    column = random.randint(0,6)
+    return column
+    
+num_players = 2
+
 def intro():
+    board = create_board()
     how_many = input(intro_banner)
     if how_many == "2":
-        play(game_over,player_turn,board)
+        play_two_player(game_over,player_turn,board)
     if how_many == "1":
-        print("Too bad, there is only a 2 player version.")
-        play(game_over,player_turn,board)
+        num_players = 1
+        # print("Too bad, there is only a 2 player version.")
+        play_one_player(game_over,player_turn,board)
     else:
-        print("You typed:",how_many, ". \nThat's not 1 or 2, silly.\nWe only have a 2 player game any way.")
-        play(game_over,player_turn,board)
+        print("You typed:",how_many, ". \nThat's not a 1 or a 2, silly.\nLet's play a two player game.")
+        play_two_player(game_over,player_turn,board)
 
 def play_again():
     play_again_input = input("Play again? (Y or N)")
     if play_again_input == "Y" or play_again_input == "y":
         board = create_board()
-        return play(game_over,player_turn,board)
+        return intro()
     else:
         print("Thanks for the game.")
         return False
@@ -94,19 +102,27 @@ def drop_piece(board,col,player):
         print("Column full. Please try another column.")
         return False
 
+def drop_piece_comp(board,col,player):
+    if is_valid_column(board,col):
+            for r in range(len(board)-1,-1,-1):
+                if board[r][col] == 0:
+                    board[r][col] = player
+                    return True
+    else:
+        col = computer_player_move()
+        drop_piece_comp(board,col,player)
+
+
 def input_column(board, player):
     player_input = 0
     while player_input >= 8 or player_input <= 0:
-    
             try:    
                 player_input = int(input("Player "+str(player)+" Choose a column (1-7):"))
-                # print(player_input)
                 if player_input > 0 and player_input < 8:
                     player_input -= 1
                     if(drop_piece(board,player_input,player)):
                         print_board(board)
                         return True
-                        # break
                     else:
                         return False
                 else:
@@ -117,10 +133,9 @@ def input_column(board, player):
 
   
 
-def play(game_over,player_turn,board):
+def play_two_player(game_over,player_turn,board):
     print_board(board)
     while not game_over:
-        # Get input for player 1
        
         if player_turn == 1:
             if input_column(board,1) == True:
@@ -130,7 +145,6 @@ def play(game_over,player_turn,board):
                     break
 
             else:
-                # input_column(board,1)
                 player_turn = 1
 
         if player_turn == 2:
@@ -141,9 +155,34 @@ def play(game_over,player_turn,board):
                     break
             else:
                 player_turn = 2
-        
 
-# print(print_board(board))
+def play_one_player(game_over,player_turn,board):
+    print_board(board)
+    while not game_over:
+       
+        if player_turn == 1:
+            if input_column(board,1) == True:
+                player_turn = 2
+                if(check_win(board)):
+                    play_again()
+                    break
+
+            else:
+                player_turn = 1
+
+        if player_turn == 2:
+            col = computer_player_move()
+            if drop_piece_comp(board,col,player_turn):
+                print_board(board)
+                player_turn = 1
+                if(check_win(board)):
+                    play_again()
+                    break
+            else:
+                player_turn = 2
+
+# Play game
+# computer_player_move()
 intro()
 
 
